217796d3b3320a0f4a0f900a6e8b8bb8b0a5078a
Nuzul <farhannuzul11@gmail.com>
2023-09-08 11:36:29 +0700

    Mengumpulkan

--- farhanNuzulNJBusAF/JBus.java
+++ farhanNuzulNJBusAF/JBus.java
@@ -26,7 +26,39 @@ public class JBus{
         return a;
     }
     
-    getOriginalPrice(int discountedPrice, float discountPercentage)
+    public static int getDiscountedPrice(int price, float discountPercentage){
+        if (discountPercentage > 100.0f){
+            discountPercentage = 100.0f;
+        } 
+        int Percentage = (int) discountPercentage;
+        int result = ((100-Percentage)*price)/100;
+        return result;
+        
+    }
+    
+    public static int getOriginalPrice(int discountedPrice, float discountPercentage){
+        int Percentage = (int) discountPercentage;
+        int result = (100*discountedPrice) / (100-Percentage);
+        return result;
+    }
+    
+    public static float getAdminFeePercentage(){
+         return 0.05f;
+    }
+    
+    public static int getAdminFee(int price){
+         float result = price*0.05f;
+         int adminFee = (int) result;
+         return adminFee;
+    }
+    
+    public static int getTotalPrice(int price, int numberOfSeat){
+        float adminFeeFloat = price*0.05f;
+        int adminFee = (int) adminFeeFloat;
+        
+        int result = (price*numberOfSeat)+(adminFee*numberOfSeat);
+        return result;
+    }
     
     public static void main (String [] args){
         int i = getBusId();
@@ -40,6 +72,21 @@ public class JBus{
         
         float f = getDiscountPercentage(1000, 900);
         System.out.println(f);
+        
+        int priceAfterDiscount = getDiscountedPrice(1000, 10.0f);
+        System.out.println(priceAfterDiscount);
+        
+        int priceBeforeDiscount = getOriginalPrice(900, 10.0f);
+        System.out.println(priceBeforeDiscount);
+        
+        float adminFee = getAdminFeePercentage();
+        System.out.println(adminFee);
+        
+        int adminFeeReal = getAdminFee(1000);
+        System.out.println(adminFeeReal);
+        
+        int totalPrice =  getTotalPrice(10000,2);
+        System.out.println(totalPrice);
     }
 }
 
