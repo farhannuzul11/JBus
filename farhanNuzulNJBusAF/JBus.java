2e9913304ad21f0efb975ac3c968afccd34c760f
Nuzul <farhannuzul11@gmail.com>
2023-10-02 22:58:40 +0700

    PT04- Selesai

--- farhanNuzulNJBusAF/JBus.java
+++ farhanNuzulNJBusAF/JBus.java
@@ -12,34 +12,41 @@ public class JBus
 
     public static void main(String[] args) {
         Bus b = createBus();
-        // Payment
         Timestamp schedule1 = Timestamp.valueOf("2023-7-18 15:00:00");
         Timestamp schedule2 = Timestamp.valueOf("2023-7-20 12:00:00");
 
         b.addSchedule(schedule1);
         b.addSchedule(schedule2);
 
-        b.schedules.forEach(Schedule :: printSchedule);
+        b.schedules.forEach(Schedule::printSchedule);
 
+        for (Schedule s: b.schedules) {
+               s.printSchedule();
+        }
+
+        //Invalid date
         Timestamp t1 = Timestamp.valueOf("2023-7-19 15:00:00");
         System.out.println("Make booking at July 19, 2023 15:00:00 Seat ER12");
-        System.out.println(Payment.makeBooking(t1, "ER12", b));
+        System.out.println(Payment.makeBooking(t1, "AF12", b));
 
+        //Valid date, invalid seat
         Timestamp t2 = Timestamp.valueOf("2023-7-18 15:00:00");
-        System.out.println("\nMake booking at July 19, 2023 15:00:00 Seat ER20");
-        System.out.println(Payment.makeBooking(t2, "ER20", b));
+        System.out.println("\nMake booking at July 18, 2023 15:00:00 Seat ER20");
+        System.out.println(Payment.makeBooking(t2, "AF20", b));
 
+        //Valid date, valid seat
         System.out.println("\nMake booking at July 18, 2023 15:00:00 Seat ER07");
-        System.out.println(Payment.makeBooking(t2, "ER07", b));
+        System.out.println(Payment.makeBooking(t2, "AF07", b));
 
         Timestamp t3 = Timestamp.valueOf("2023-7-20 12:00:00");
         System.out.println("\nMake booking at July 20, 2023 12:00:00 Seat ER01");
-        System.out.println(Payment.makeBooking(t3, "ER01", b));
+        System.out.println(Payment.makeBooking(t3, "AF01", b));
 
         System.out.println("\nMake booking at July 20, 2023 12:00:00 Seat ER01 again");
-        System.out.println(Payment.makeBooking(t3, "ER01", b));
+        System.out.println(Payment.makeBooking(t3, "AF01", b));
 
+        //Check if the data changes
         System.out.println("\nUpdated Schedule\n");
-        b.schedules.forEach(Schedule :: printSchedule);
+        b.schedules.forEach(Schedule::printSchedule);
     }
 }
