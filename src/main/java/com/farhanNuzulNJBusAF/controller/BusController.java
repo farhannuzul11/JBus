be6d3811d412bdb3c3f435f076409dd4f8c3a4da
Nuzul <farhannuzul11@gmail.com>
2023-11-23 22:39:38 +0700

    TP09 - Selesai

--- src/main/java/com/farhanNuzulNJBusAF/controller/BusController.java
+++ src/main/java/com/farhanNuzulNJBusAF/controller/BusController.java
@@ -1,74 +1,143 @@
 package com.farhanNuzulNJBusAF.controller;
 
 import com.farhanNuzulNJBusAF.*;
-import com.farhanNuzulNJBusAF.controller.BaseResponse;
-import com.farhanNuzulNJBusAF.controller.BasicGetController;
 import com.farhanNuzulNJBusAF.dbjson.JsonAutowired;
 import com.farhanNuzulNJBusAF.dbjson.JsonTable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.farhanNuzulNJBusAF.Algorithm;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @RestController
 @RequestMapping("/bus")
-public class BusController /*implements BasicGetController*/ {
-    public static @JsonAutowired(value = Account.class, filepath = "C:\\Users\\asus\\OneDrive\\Dokumen\\Semester 3\\OOP\\OOP 01\\JBus\\src\\main\\java\\com\\farhanNuzulNJBusAF\\json\\account.json") JsonTable<Account> busTable;
+public class BusController implements BasicGetController<Bus> {
+    public static @JsonAutowired(value = Account.class, filepath = "src\\main\\java\\com\\farhanNuzulNJBusAF\\json\\bus.json") JsonTable<Bus> busTable;
 
-    @PostMapping("/create")
-    public BaseResponse<Bus> create (
-        @RequestParam int accountId,
-        @RequestParam String name,
-        @RequestParam int capacity,
-        @RequestParam List<Facility> facilities,
-        @RequestParam BusType busType,
-        @RequestParam int pricee,
-        @RequestParam int statioDepartureId,
-        @RequestParam int statioArrivalId
+    public BusController() {}
 
-    ) {
-//        Station departureStation = null;
-//        Station arrivalStation = null;
-//        Price priceResult = null;
-//
-//        Predicate<Account> predAcc = a -> a.id == accountId && a.company != null;
-//        boolean valid1 = Algorithm.exists(AccountController.accountTable, predAcc);
-//        Predicate<Bus> predBus = b -> b.departure == departureStation && b.arrival == arrivalStation;
-//        boolean valid2 = Algorithm.exists(BusController.busTable, predBus);
-//
-//        if (valid1 && valid2) {
-//            Bus bus = new Bus(name, facilities, priceResult, capacity, busType, departureStation, arrivalStation, accountId);
-//           busTable.add(bus);
-//            return new BaseResponse<>(true, "Dapat menambahkan bus", bus);
-//        } else {
-//            return new BaseResponse<>(false, "Gagal menambahkan bus", null);
-//        }
-//    }
-    return null;
+    public JsonTable<Bus> getJsonTable() {
+        return this.busTable;
     }
 
+    @PostMapping("/create")
+    public BaseResponse<Bus> create(
+            @RequestParam int accountId,
+            @RequestParam String name,
+            @RequestParam int capacity,
+            @RequestParam List<Facility> facilities,
+            @RequestParam BusType busType,
+            @RequestParam int price,
+            @RequestParam int stationDepartureId,
+            @RequestParam int stationArrivalId
+    ) {
+        Predicate<Account> predAcc = a -> a.id == accountId && a.company != null;
+        boolean valid1 = Algorithm.exists(AccountController.accountTable, predAcc);
+        Predicate<Station> predStation = s -> s.id == stationDepartureId || s.id == stationArrivalId;
+        boolean valid2 = Algorithm.exists(StationController.stationTable, predStation);
+
+        if(valid1 && valid2) {
+            Predicate<Station> DepStation = s -> s.id == stationDepartureId;
+            Predicate<Station> ArrStation = s -> s.id == stationArrivalId;
+            Bus bus = new Bus(name, facilities, new Price(price), capacity, busType, Algorithm.find(StationController.stationTable, DepStation), Algorithm.find(StationController.stationTable, ArrStation), accountId);
+            busTable.add(bus);
+            return new BaseResponse<>(true, "Berhasil menambahkan bus", bus);
+        } else {
+            return new BaseResponse<>(false, "Gagal menambahkan bus", null);
+        }
+    }
+
+    @PostMapping("/addSchedule")
     public BaseResponse<Bus> addSchedule(
             @RequestParam int busId,
             @RequestParam String time
     ) {
-//        Bus bus = busTable.getById(busId);
-//
-//        if (bus != null) {
-//            try {
-////                bus.addSchedule(time);
-//                return new BaseResponse<>(true, "Berhasil menambahkan schedule", bus);
-//            } catch (Exception e) {
-//                return new BaseResponse<>(false, "Gagal menambahkan schedule: " + e.getMessage(), null);
-//            }
-//        } else {
-//            return new BaseResponse<>(false, "Bus tidak ditemukan", null);
-//        }
-        return null;
+        Predicate<Bus> pred = b -> b.id == busId;
+        Bus b = Algorithm.find(getJsonTable(), pred);
+        if(b != null) {
+            Timestamp timestamp = Timestamp.valueOf(time);
+            b.addSchedule(timestamp);
+            return new BaseResponse<>(true, "Berhasil membuat schedule", b);
+        }
+        return new BaseResponse<>(false, "Gagal membuat schedule", null);
     }
-
 }
+
+//package com.farhanNuzulNJBusAF.controller;
+//
+//import com.farhanNuzulNJBusAF.*;
+//import com.farhanNuzulNJBusAF.controller.BaseResponse;
+//import com.farhanNuzulNJBusAF.controller.BasicGetController;
+//import com.farhanNuzulNJBusAF.dbjson.JsonAutowired;
+//import com.farhanNuzulNJBusAF.dbjson.JsonTable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import com.farhanNuzulNJBusAF.Algorithm;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/bus")
+//public class BusController /*implements BasicGetController*/ {
+//    public static @JsonAutowired(value = Account.class, filepath = "C:\\Users\\asus\\OneDrive\\Dokumen\\Semester 3\\OOP\\OOP 01\\JBus\\src\\main\\java\\com\\farhanNuzulNJBusAF\\json\\station.json") JsonTable<Account> busTable;
+//
+//    @PostMapping("/create")
+//    public BaseResponse<Bus> create (
+//        @RequestParam int accountId,
+//        @RequestParam String name,
+//        @RequestParam int capacity,
+//        @RequestParam List<Facility> facilities,
+//        @RequestParam BusType busType,
+//        @RequestParam int pricee,
+//        @RequestParam int statioDepartureId,
+//        @RequestParam int statioArrivalId
+//
+//    ) {
+////        Station departureStation = null;
+////        Station arrivalStation = null;
+////        Price priceResult = null;
+////
+////        Predicate<Account> predAcc = a -> a.id == accountId && a.company != null;
+////        boolean valid1 = Algorithm.exists(AccountController.accountTable, predAcc);
+////        Predicate<Bus> predBus = b -> b.departure == departureStation && b.arrival == arrivalStation;
+////        boolean valid2 = Algorithm.exists(BusController.busTable, predBus);
+////
+////        if (valid1 && valid2) {
+////            Bus bus = new Bus(name, facilities, priceResult, capacity, busType, departureStation, arrivalStation, accountId);
+////           busTable.add(bus);
+////            return new BaseResponse<>(true, "Dapat menambahkan bus", bus);
+////        } else {
+////            return new BaseResponse<>(false, "Gagal menambahkan bus", null);
+////        }
+////    }
+//    return null;
+//    }
+//
+//    public BaseResponse<Bus> addSchedule(
+//            @RequestParam int busId,
+//            @RequestParam String time
+//    ) {
+////        Bus bus = busTable.getById(busId);
+////
+////        if (bus != null) {
+////            try {
+//////                bus.addSchedule(time);
+////                return new BaseResponse<>(true, "Berhasil menambahkan schedule", bus);
+////            } catch (Exception e) {
+////                return new BaseResponse<>(false, "Gagal menambahkan schedule: " + e.getMessage(), null);
+////            }
+////        } else {
+////            return new BaseResponse<>(false, "Bus tidak ditemukan", null);
+////        }
+//        return null;
+//    }
+//
+//}
+
+
 
 
--- src/main/java/com/farhanNuzulNJBusAF/controller/PaymentController.java
+++ src/main/java/com/farhanNuzulNJBusAF/controller/PaymentController.java
@@ -1,38 +1,89 @@
 package com.farhanNuzulNJBusAF.controller;
 
-import com.farhanNuzulNJBusAF.Account;
-import com.farhanNuzulNJBusAF.BusType;
-import com.farhanNuzulNJBusAF.Facility;
-import com.farhanNuzulNJBusAF.Payment;
-import com.farhanNuzulNJBusAF.dbjson.JsonAutowired;
+import com.farhanNuzulNJBusAF.*;
 import com.farhanNuzulNJBusAF.dbjson.JsonTable;
+import com.farhanNuzulNJBusAF.dbjson.JsonAutowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @RestController
 @RequestMapping("/payment")
-public class PaymentController /*implements BasicGetController*/{
-    public static @JsonAutowired(value = Account.class, filepath = "C:\\Users\\asus\\OneDrive\\Dokumen\\Semester 3\\OOP\\OOP 01\\JBus\\src\\main\\java\\com\\farhanNuzulNJBusAF\\json\\account.json") JsonTable<Account> paymentTable;
+public class PaymentController implements BasicGetController<Payment> {
+    public static @JsonAutowired(value = Account.class, filepath = "src\\main\\java\\com\\auliaAnugrahAzizJBusRD\\json\\payment.json") JsonTable<Payment> paymentTable;
 
-    @RequestMapping(value="/makeBooking", method=RequestMethod.POST)
+    public PaymentController() {}
+
+    public JsonTable<Payment> getJsonTable() {
+        return this.paymentTable;
+    }
+
+    @RequestMapping(value="/makeBooking", method= RequestMethod.POST)
     public BaseResponse<Payment> makeBooking(
-    @RequestParam int buyerId,
-    @RequestParam int renterId,
-    @RequestParam int busId,
-    @RequestParam List<String> busSeats,
-    @RequestParam String departure
-    ){
-        return null;
+            @RequestParam int buyerId,
+            @RequestParam int renterId,
+            @RequestParam int busId,
+            @RequestParam List<String> busSeats,
+            @RequestParam String departureDate
+    ) {
+        Timestamp timestamp = Timestamp.valueOf(departureDate);
+        Predicate<Account> predAccount = a -> a.id == buyerId;
+        Predicate<Bus> predBus = b -> b.id == busId;
+
+        Account acc = Algorithm.find(AccountController.accountTable, predAccount);
+        Bus bus = Algorithm.find(BusController.busTable, predBus);
+
+        boolean statusSched = false;
+
+        for(int i = 0; i < bus.schedules.size(); i++) {
+            if(bus.schedules.get(i).departureSchedule.equals(timestamp)) {
+                System.out.println(bus.schedules.get(i).departureSchedule);
+                statusSched = true;
+            }
+        }
+
+        Payment payment = null;
+        if(acc != null && bus != null && statusSched) {
+            if(acc.balance >= bus.price.price * busSeats.size()) {
+                payment = new Payment(buyerId, renterId, busId, busSeats, timestamp);
+            } else {
+                return new BaseResponse<>(false, "Kekurangan saldo", payment);
+            }
+        }
+
+        if(payment == null) {
+            return new BaseResponse<>(false, "Gagal membuat booking", payment);
+        } else {
+            payment.makeBooking(timestamp, busSeats, bus);
+            payment.status = Invoice.PaymentStatus.WAITING;
+            paymentTable.add(payment);
+            return new BaseResponse<>(true, "Berhasil membuat booking", payment);
+        }
     }
 
-    @RequestMapping(value="/{id}/accept", method=RequestMethod.POST)
-    public BaseResponse<Payment> accept(@PathVariable int id){
-        return null;
+    @RequestMapping(value="/{id}/accept", method= RequestMethod.POST)
+    public BaseResponse<Payment> accept(@PathVariable int id) {
+        Predicate<Payment> pred = p -> p.id == id;
+        Payment payment = null;
+        if(Algorithm.exists(getJsonTable(), pred)) {
+            payment = Algorithm.find(getJsonTable(), pred);
+            payment.status = Invoice.PaymentStatus.SUCCESS;
+            return new BaseResponse<>(true, "Sukses menerima booking", payment);
+        }
+        return new BaseResponse<>(false, "Gagal menerima booking", payment);
     }
+
     @RequestMapping(value="/{id}/cancel", method=RequestMethod.POST)
-    public BaseResponse<Payment> cancel(@PathVariable int id){
-        return null;
+    public BaseResponse<Payment> cancel(@PathVariable int id) {
+        Predicate<Payment> pred = p -> p.id == id;
+        Payment payment = null;
+        if(Algorithm.exists(getJsonTable(), pred)) {
+            payment = Algorithm.find(getJsonTable(), pred);
+            payment.status = Invoice.PaymentStatus.FAILED;
+            return new BaseResponse<>(true, "Sukses meng-cancel booking", payment);
+        }
+        return new BaseResponse<>(false, "Gagal meng-cancel booking", payment);
     }
 
 }
